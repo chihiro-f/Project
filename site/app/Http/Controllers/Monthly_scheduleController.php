f5e3a3dbca876312155416dcf2de92ba3628cf9a
chihiro-f <cfnacchi@gmail.com>
2021-09-04 19:55:44 +0900

    edit&update完了

--- site/app/Http/Controllers/Monthly_scheduleController.php
+++ site/app/Http/Controllers/Monthly_scheduleController.php
@@ -3,26 +3,30 @@
 namespace App\Http\Controllers;
 
 use App\Monthly_schedule;
-// use Illuminate\Http\Request;
 use App\Http\Requests\Monthly_scheduleRequest;
+use Illuminate\Http\Request;
 
 class Monthly_scheduleController extends Controller
 {
     //
-    // public function index(Monthly_schedule $monthly_schedule){
-    //     return view('Monthly_schedule.index')->with(['monthly_schedules' => $monthly_schedule->get()]);  
-    // }
+
     public function show(Monthly_schedule $monthly_schedule){
         return view('Monthly_schedule.show')->with([ 'monthly_schedules' => $monthly_schedule ]);
     }
+    
+    public function store(Today_scheduleRequest $request, Today_schedule $today_schedule){
+        $input = $request['today_schedule'];
+        $today_schedule->fill($input)->save();
+        return redirect('/today/' . $today_schedule->id);
+    }
+    
     public function edit(Monthly_schedule $monthly_schedule){
-        return view('Monthly_schedule.edit')->with(['monthly_schedule'=>$monthly_schedule]);
+        return view('Monthly_schedule.edit')->with(['monthly_schedule' => $monthly_schedule]);
     }
 
-    public function update(Monthly_schedule $request, Monthly_schedule $monthly_schedule){
-        $input_monthly=$request['monthly_schedule'];
-        $monthly->fill($input_monthly)->save();
-
-        return redirect('/monthly_schedules/'.$monthly_schedule->id);
+    public function update(Request $request, Monthly_schedule $monthly_schedule){
+        $input_monthly = $request['monthly_schedule'];
+        $monthly_schedule->fill($input_monthly)->save();
+        return redirect('/monthly_schedule/' . $monthly_schedule->id);
     }
 }
--- site/resources/views/Today_schedule/create.blade.php
+++ site/resources/views/Today_schedule/create.blade.php
@@ -10,6 +10,15 @@
 
   <body>
     <h2 class = 'title_cite'>部員サイト</h2>
+    
+    <header>
+      <ul>
+        <li><a href="/home">Home</a></li>
+        <li><a href="/monthly_schedule/1">今後の予定一覧</a></li>
+        <li><a href="/record">練習録音</a></li>
+        <li><a href="/network">連絡網</a></li>
+      </ul>
+    </header>
 
     <form action="/today" method="POST">
     @csrf
@@ -17,12 +26,12 @@
       <h1 class = 'title'>新規投稿_今日のスケジュール</h1>
       <!-- タイトル入力 -->
       <h4>タイトル</h4>
-      <input type="text" name="today_schedules[title]" placeholder="タイトルを入力" value="{{ old('today_schedule.title') }}" >
+      <input type="text" name="today_schedule[title]" placeholder="タイトルを入力" value="{{ old('today_schedule.title') }}" >
       <p class="title_error" style="color:red">{{ $errors->first('today_schedule.title') }}</p>
       
       <!-- スケジュール入力 -->
       <h4>スケジュール内容</h4>
-      <textarea name="today_schedules[content]" placeholder="本日のスケジュール内容を記入してください" value="{{ old('today_schedule.content') }}"></textarea>
+      <textarea name="today_schedule[content]" placeholder="本日のスケジュール内容を記入してください" value="{{ old('today_schedule.content') }}"></textarea>
       <p class="body_error" style="color:red">{{ $errors->first('today_schedule.content') }}</p>
       <br><br>
       <input type="submit" value="投稿" />
@@ -31,7 +40,7 @@
     <p></p>
     </form>
 
-    <a href="/today/{{ $today_schedule->id }}">戻る</a>
+    <a href="/today">戻る</a>
   </body>
 
 </html>
